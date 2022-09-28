ca2a08fe1bd17e7ea9227d2f2e5925bb436575c3
Rasul Isk <rasul_iskenderov@mail.ru>
2022-09-28 18:47:53 +0300

    [Part 1 - Passed] second test added and passed

--- lib/yahtzee.ex
+++ lib/yahtzee.ex
@@ -1,7 +1,11 @@
 defmodule Yahtzee do
-  def score_upper(dice), do: %{
-    Ones: length(Enum.filter(dice, fn e -> e == 1 end)),
-    Twos: length(Enum.filter(dice, fn e -> e == 2 end))
-}
-
+  def score_upper(dice),
+    do: %{
+      Ones: length(Enum.filter(dice, fn e -> e == 1 end)),
+      Twos: length(Enum.filter(dice, fn e -> e == 2 end)),
+      Threes: length(Enum.filter(dice, fn e -> e == 3 end)),
+      Fours: length(Enum.filter(dice, fn e -> e == 4 end)),
+      Fives: length(Enum.filter(dice, fn e -> e == 5 end)),
+      Sixes: length(Enum.filter(dice, fn e -> e == 6 end))
+    }
 end
--- test/yahtzee_upper_section_test.exs
+++ test/yahtzee_upper_section_test.exs
@@ -5,14 +5,14 @@ defmodule YahtzeeUpperSectionTest do
   def generate(die_face, occurrences) do
     Enum.to_list(1..6)
     |> List.delete(die_face)
-    |> Enum.shuffle
+    |> Enum.shuffle()
     |> Enum.take(5 - occurrences)
     |> Enum.concat(List.duplicate(die_face, occurrences))
-    |> Enum.shuffle
+    |> Enum.shuffle()
   end
 
   test "works with 1 one" do
-    assert %{Ones: 1} = Yahtzee.score_upper([1,2,3,4,5])
+    assert %{Ones: 1} = Yahtzee.score_upper([1, 2, 3, 4, 5])
   end
 
   test "works with any combination of dice, containing 1-5 ones" do
@@ -20,7 +20,13 @@ defmodule YahtzeeUpperSectionTest do
   end
 
   test "works with twos" do
-  Enum.map(1..5, fn n -> assert %{Twos: ^n} = Yahtzee.score_upper(generate(2, n)) end)
-end
+    Enum.map(1..5, fn n -> assert %{Twos: ^n} = Yahtzee.score_upper(generate(2, n)) end)
+  end
 
+  test "works on upper section, with all the other cases" do
+    Enum.map(1..5, fn n -> assert %{Threes: ^n} = Yahtzee.score_upper(generate(3, n)) end)
+    Enum.map(1..5, fn n -> assert %{Fours: ^n} = Yahtzee.score_upper(generate(4, n)) end)
+    Enum.map(1..5, fn n -> assert %{Fives: ^n} = Yahtzee.score_upper(generate(5, n)) end)
+    Enum.map(1..5, fn n -> assert %{Sixes: ^n} = Yahtzee.score_upper(generate(6, n)) end)
+  end
 end
